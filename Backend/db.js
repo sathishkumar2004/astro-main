a639b72d62d1561c9abefc2281aac26679bf9e8c
Sathishkumar <ssathishkumar0009@gmail.com>
2025-11-17 16:11:21 +0530

    updated project

--- Backend/db.js
+++ Backend/db.js
@@ -7,15 +7,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'postgres',
     logging: false,
-    dialectOptions: {
+    dialectOptions: process.env.NODE_ENV === "production" ? {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // Important for Render
+        rejectUnauthorized: false
       }
-    }
+    } : {}
   }
 );
 
